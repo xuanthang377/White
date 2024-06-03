89ffb0c5b52075755349f948ea4b9a02af45a27f
xuanthang377 <nguyenxuanthang377@gmail.com>
2024-06-04 00:32:27 +0700

    update lan cuoi

---
+++ mcu1/main/LCD.c
@@ -0,0 +1,427 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <math.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+
+#include "../Lib/ili9340.h"
+#include "../Lib/fontx.h"
+
+#include "../Lib/decode_png.h"
+#include "../Lib/pngle.h"
+#include "../Lib/LCD.h"
+#include "driver/gpio.h"
+#include "../Lib/bmpfile.h"
+
+#define INTERVAL 400
+#define WAIT vTaskDelay(INTERVAL)
+
+static const char *TAG = "MAIN";
+
+TickType_t BMPTest(TFT_t * dev, char * file, int width, int height) {
+	TickType_t startTick, endTick, diffTick;
+	startTick = xTaskGetTickCount();
+
+	lcdFillScreen(dev, BLACK);
+
+	// open BMP file
+	esp_err_t ret;
+	FILE* fp = fopen(file, "rb");
+	if (fp == NULL) {
+		ESP_LOGW(__FUNCTION__, "File not found [%s]", file);
+		return 0;
+	}
+
+	// read bmp header
+	bmpfile_t *result = (bmpfile_t*)malloc(sizeof(bmpfile_t));
+	if (result == NULL) {
+		ESP_LOGE(__FUNCTION__, "malloc fail");
+		return 0;
+	}
+
+	ret = fread(result->header.magic, 1, 2, fp);
+	assert(ret == 2);
+	ESP_LOGD(__FUNCTION__,"result->header.magic=%c %c", result->header.magic[0], result->header.magic[1]);
+	if (result->header.magic[0]!='B' || result->header.magic[1] != 'M') {
+		ESP_LOGW(__FUNCTION__, "File is not BMP");
+		free(result);
+		fclose(fp);
+		return 0;
+	}
+	ret = fread(&result->header.filesz, 4, 1 , fp);
+	assert(ret == 1);
+	ESP_LOGD(__FUNCTION__,"result->header.filesz=%"PRIu32, result->header.filesz);
+	ret = fread(&result->header.creator1, 2, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->header.creator2, 2, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->header.offset, 4, 1, fp);
+	assert(ret == 1);
+
+	// read dib header
+	ret = fread(&result->dib.header_sz, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.width, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.height, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.nplanes, 2, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.depth, 2, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.compress_type, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.bmp_bytesz, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.hres, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.vres, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.ncolors, 4, 1, fp);
+	assert(ret == 1);
+	ret = fread(&result->dib.nimpcolors, 4, 1, fp);
+	assert(ret == 1);
+
+	if((result->dib.depth == 24) && (result->dib.compress_type == 0)) {
+		// BMP rows are padded (if needed) to 4-byte boundary
+		uint32_t rowSize = (result->dib.width * 3 + 3) & ~3;
+		int w = result->dib.width;
+		int h = result->dib.height;
+		ESP_LOGD(__FUNCTION__,"w=%d h=%d", w, h);
+		int _x;
+		int _w;
+		int _cols;
+		int _cole;
+		if (width >= w) {
+			_x = (width - w) / 2;
+			_w = w;
+			_cols = 0;
+			_cole = w - 1;
+		} else {
+			_x = 0;
+			_w = width;
+			_cols = (w - width) / 2;
+			_cole = _cols + width - 1;
+		}
+		ESP_LOGD(__FUNCTION__,"_x=%d _w=%d _cols=%d _cole=%d",_x, _w, _cols, _cole);
+
+		int _y;
+		int _rows;
+		int _rowe;
+		if (height >= h) {
+			_y = (height - h) / 2;
+			_rows = 0;
+			_rowe = h -1;
+		} else {
+			_y = 0;
+			_rows = (h - height) / 2;
+			_rowe = _rows + height - 1;
+		}
+		ESP_LOGD(__FUNCTION__,"_y=%d _rows=%d _rowe=%d", _y, _rows, _rowe);
+
+#define BUFFPIXEL 20
+		uint8_t sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+		uint16_t *colors = (uint16_t*)malloc(sizeof(uint16_t) * w);
+		if (colors == NULL) {
+			ESP_LOGE(__FUNCTION__, "malloc fail");
+			free(result);
+			fclose(fp);
+			return 0;
+		}
+
+		for (int row=0; row<h; row++) { // For each scanline...
+			if (row < _rows || row > _rowe) continue;
+			// Seek to start of scan line.	It might seem labor-
+			// intensive to be doing this on every line, but this
+			// method covers a lot of gritty details like cropping
+			// and scanline padding.	Also, the seek only takes
+			// place if the file position actually needs to change
+			// (avoids a lot of cluster math in SD library).
+			// Bitmap is stored bottom-to-top order (normal BMP)
+			int pos = result->header.offset + (h - 1 - row) * rowSize;
+			fseek(fp, pos, SEEK_SET);
+			int buffidx = sizeof(sdbuffer); // Force buffer reload
+
+			int index = 0;
+			for (int col=0; col<w; col++) { // For each pixel...
+				if (buffidx >= sizeof(sdbuffer)) { // Indeed
+					fread(sdbuffer, sizeof(sdbuffer), 1, fp);
+					buffidx = 0; // Set index to beginning
+				}
+				if (col < _cols || col > _cole) continue;
+				// Convert pixel from BMP to TFT format, push to display
+				uint8_t b = sdbuffer[buffidx++];
+				uint8_t g = sdbuffer[buffidx++];
+				uint8_t r = sdbuffer[buffidx++];
+				colors[index++] = rgb565(r, g, b);
+			} // end for col
+			ESP_LOGD(__FUNCTION__,"lcdDrawMultiPixels row=%d",row);
+			//lcdDrawMultiPixels(dev, _x, row+_y, _w, colors);
+			lcdDrawMultiPixels(dev, _x, _y, _w, colors);
+			_y++;
+		} // end for row
+		free(colors);
+	} // end if 
+	free(result);
+	fclose(fp);
+
+	endTick = xTaskGetTickCount();
+	diffTick = endTick - startTick;
+	ESP_LOGI(__FUNCTION__, "elapsed time[ms]:%"PRIu32,diffTick*portTICK_PERIOD_MS);
+	return diffTick;
+}
+
+
+void ArrowTest(TFT_t * dev, FontxFile *fx, uint16_t model, int width, int height, uint8_t ascii[24],int a,int b) {
+   
+
+    // get font width & height
+    uint8_t buffer[FontxGlyphBufSize];
+    uint8_t fontWidth;
+    uint8_t fontHeight;
+    GetFontx(fx, 0, buffer, &fontWidth, &fontHeight);
+    ESP_LOGD(__FUNCTION__, "fontWidth=%d fontHeight=%d", fontWidth, fontHeight);
+    
+    uint16_t xpos;
+    uint16_t ypos;
+    int stlen;
+    uint16_t color;
+
+    //lcdFillScreen(dev, BLACK);
+    lcdSetFontDirection(dev, DIRECTION0);
+	if ( b == 30 )
+    {color = WHITE;}
+	if ( b == 65 )
+    {color = YELLOW;}
+	if ( b == 95 )
+    {color = GREEN;}
+	if ( b == 125 )
+    {color = WHITE;}
+	if ( b == 155 )
+    {color = YELLOW;}
+    lcdDrawString(dev, fx, a, b, ascii, color);
+
+}
+void PNGTest(TFT_t * dev, char * file, int width, int height) {
+	// TickType_t startTick, endTick, diffTick;
+	// startTick = xTaskGetTickCount();
+
+	// lcdFillScreen(dev, BLACK);
+
+	int _width = width;
+	if (width > 240) _width = 240;
+	int _height = height;
+	if (height > 320) _height = 320;
+
+	// open PNG file
+	FILE* fp = fopen(file, "rb");
+	
+
+	char buf[1024];
+	size_t remain = 0;
+	int len;
+
+	pngle_t *pngle = pngle_new(_width, _height);
+	
+
+	pngle_set_init_callback(pngle, png_init);
+	pngle_set_draw_callback(pngle, png_draw);
+	pngle_set_done_callback(pngle, png_finish);
+
+	double display_gamma = 2.2;
+	pngle_set_display_gamma(pngle, display_gamma);
+
+
+	while (!feof(fp)) {
+		if (remain >= sizeof(buf)) {
+			ESP_LOGE(__FUNCTION__, "Buffer exceeded");
+		}
+
+		len = fread(buf + remain, 1, sizeof(buf) - remain, fp);
+		if (len <= 0) {
+			//printf("EOF\n");
+			break;
+		}
+
+		int fed = pngle_feed(pngle, buf, remain + len);
+		if (fed < 0) {
+			ESP_LOGE(__FUNCTION__, "ERROR; %s", pngle_error(pngle));
+		}
+
+		remain = remain + len - fed;
+		if (remain > 0) memmove(buf, buf + fed, remain);
+	}
+
+	fclose(fp);
+
+	uint16_t pngWidth = width;
+	uint16_t offsetX = 0;
+	if (width > pngle->imageWidth) {
+		pngWidth = pngle->imageWidth;
+		offsetX = (width - pngle->imageWidth) / 2;
+	}
+	ESP_LOGD(__FUNCTION__, "pngWidth=%d offsetX=%d", pngWidth, offsetX);
+
+	uint16_t pngHeight = height;
+	uint16_t offsetY = 0;
+	if (height > pngle->imageHeight) {
+		pngHeight = pngle->imageHeight;
+		offsetY = (height - pngle->imageHeight) / 2;
+	}
+	ESP_LOGD(__FUNCTION__, "pngHeight=%d offsetY=%d", pngHeight, offsetY);
+	uint16_t *colors = (uint16_t*)malloc(sizeof(uint16_t) * pngWidth);
+	
+
+
+
+	for(int y = 0; y < pngHeight; y++){
+		for(int x = 0;x < pngWidth; x++){
+			//pixel_png pixel = pngle->pixels[y][x];
+			//colors[x] = rgb565(255, 255,   0);
+			colors[x] = pngle->pixels[y][x];
+		}
+		lcdDrawMultiPixels(dev, offsetX, y+offsetY, pngWidth, colors);
+		//vTaskDelay(1);
+	}
+	free(colors);
+	pngle_destroy(pngle, _width, _height);
+
+}
+
+void ShowPngImage(TFT_t * dev, char * file, int width, int height, int xpos, int ypos) {
+	// open PNG file
+	FILE* fp = fopen(file, "rb");
+	if (fp == NULL) {
+		ESP_LOGW(__FUNCTION__, "File not found [%s]", file);
+		return;
+	}
+
+	char buf[1024];
+	size_t remain = 0;
+	int len;
+
+	int _width = width;
+	if (width > 240) _width = 240;
+	int _height = height;
+	if (height > 320) _height = 320;
+	pngle_t *pngle = pngle_new(_width, _height);
+
+	pngle_set_init_callback(pngle, png_init);
+	pngle_set_draw_callback(pngle, png_draw);
+	pngle_set_done_callback(pngle, png_finish);
+
+	double display_gamma = 2.2;
+	pngle_set_display_gamma(pngle, display_gamma);
+
+
+	while (!feof(fp)) {
+		if (remain >= sizeof(buf)) {
+			ESP_LOGE(__FUNCTION__, "Buffer exceeded");
+			while(1) vTaskDelay(1);
+		}
+
+		len = fread(buf + remain, 1, sizeof(buf) - remain, fp);
+		if (len <= 0) {
+			//printf("EOF\n");
+			break;
+		}
+
+		int fed = pngle_feed(pngle, buf, remain + len);
+		if (fed < 0) {
+			ESP_LOGE(__FUNCTION__, "ERROR; %s", pngle_error(pngle));
+			while(1) vTaskDelay(1);
+		}
+
+		remain = remain + len - fed;
+		if (remain > 0) memmove(buf, buf + fed, remain);
+	}
+
+	fclose(fp);
+
+	uint16_t pngWidth = pngle_get_width(pngle);
+	uint16_t pngHeight = pngle_get_height(pngle);
+	ESP_LOGD(__FUNCTION__, "pngWidth=%d pngHeight=%d", pngWidth, pngHeight);
+	int _xpos = xpos - (pngHeight/2);
+	int _ypos = ypos - (pngWidth/2);
+	ESP_LOGD(__FUNCTION__, "xpos=%d ypos=%d _xpos=%d _ypos=%d", xpos, ypos, _xpos, _ypos);
+	uint16_t *colors = (uint16_t*)malloc(sizeof(uint16_t) * pngWidth);
+	if (colors == NULL) {
+		ESP_LOGE(__FUNCTION__, "malloc fail");
+		pngle_destroy(pngle, _width, _height);
+		return;
+	}
+
+	for(int y = 0; y < pngHeight; y++){
+		for(int x = 0;x < pngWidth; x++){
+			//pixel_png pixel = pngle->pixels[y][x];
+			//colors[x] = rgb565(pixel.red, pixel.green, pixel.blue);
+			colors[x] = pngle->pixels[y][x];
+		}
+		lcdDrawMultiPixels(dev, _xpos, y+_ypos, pngWidth, colors);
+		vTaskDelay(1);
+	}
+	free(colors);
+	pngle_destroy(pngle, _width, _height);
+	return;
+}
+
+
+
+
+ void listSPIFFS(char * path) {
+	DIR* dir = opendir(path);
+	assert(dir != NULL);
+	while (true) {
+		struct dirent*pe = readdir(dir);
+		if (!pe) break;
+		ESP_LOGI(__FUNCTION__,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
+	}
+	closedir(dir);
+}
+
+esp_err_t mountSPIFFS(char * path, char * label, int max_files) {
+	esp_vfs_spiffs_conf_t conf = {
+		.base_path = path,
+		.partition_label = label,
+		.max_files = max_files,
+		.format_if_mount_failed =true
+	};
+
+	
+	esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+	if (ret != ESP_OK) {
+		if (ret ==ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount or format filesystem");
+		} else if (ret== ESP_ERR_NOT_FOUND) {
+			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
+		}
+		return ret;
+	}
+
+
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(conf.partition_label, &total, &used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG,"Failed to get SPIFFS partition information (%s)",esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG,"Mount %s to %s success", path, label);
+		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
+	}
+
+	return ret;
+}
+
+
+
---
+++ mcu1/main/main.c
@@ -0,0 +1,421 @@
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <math.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+#include "driver/gpio.h"
+#include <stdlib.h>
+#include "esp_spi_flash.h"
+#include "sdkconfig.h"
+#include <time.h> 
+#include "../Lib/GPS.h"
+#include "../Lib/ili9340.h"
+#include "../Lib/fontx.h"
+#include "../Lib/decode_png.h"
+#include "../Lib/pngle.h"
+#include "../Lib/LCD.h"
+#include "../Lib/SDCard.h"
+#include "../Lib/i2cdev.h"
+#include "../Lib/ds3231.h"
+#include "../Lib/mypH.h"
+#include "../Lib/myEC.h"
+#include "../Lib/ADS1115.h"
+#include "../Lib/nvs_interface.h"
+#include "../Lib/myDO.h"
+#include "../Lib/myTemp.h"
+#include "../Lib/UART.h"
+#include "../Lib/bmpfile.h"
+nvs_handle_t nvsHandle;
+TaskHandle_t test_ads1115;
+TaskHandle_t test_ds3231;
+TaskHandle_t p_sd_card;
+TaskHandle_t get_data;
+TaskHandle_t send_data;
+
+volatile char buff[126];
+volatile char buff_sd[126];
+volatile char name_file[50];
+volatile char time_buff[50];
+volatile char time_buff1[50];
+volatile data_type data;
+GPS_data gps_data;
+TFT_t dev;
+i2c_dev_t dev1;
+struct tm time1 ;
+double checkKD = 0;
+double checkVD = 0;
+double hieukd ;
+double hieuvd ;
+
+double checkDO = 0;
+double hieuDO ;
+
+double checkpH = 0;
+double hieuPH ;
+
+double checkEC = 0;
+double hieuEC ;
+
+double checkTemp = 0;
+double hieuTemp ;
+
+
+
+#define pH_ON_PIN1 GPIO_NUM_35
+#define DO_ON_PIN1 GPIO_NUM_36
+#define EC_ON_PIN1 GPIO_NUM_37
+
+
+#define INTERVAL 400
+#define WAIT vTaskDelay(INTERVAL)
+static const char *TAG = "MAIN";
+
+void send_data_task(void *arg)
+{
+    uart_write_bytes(UART_UART, buff, strlen(buff));
+	ESP_LOGI(TAG, "UART init success!\n");
+	vTaskDelete(NULL);
+}
+
+static void GPS(void *pvParameters)
+{
+	
+	gps_data = gps_get_value();	
+	
+	/* code *///0.000145
+    vTaskDelete(NULL);
+}
+
+void sd_task(void *arg)
+{
+	
+	esp_err_t err_init = sd_card_intialize();
+		//sprintf(name_file, "log_3_7");
+	if (name_file == NULL)
+	{
+		sprintf(name_file, "null.txt");
+	}
+	strcpy(buff_sd, buff);
+	strcat(buff_sd, "\n");
+	printf("buff_sd:%s", buff_sd);
+	esp_err_t err = sd_write_file(name_file, buff_sd);
+	if (err != ESP_OK)
+	{
+		ESP_LOGI(__func__, "Save data error");
+	}
+	else
+	{
+		ESP_LOGI(__func__, "Save data success");
+	}
+	if (err_init == ESP_OK)
+	{
+		sd_deinitialize();
+	}
+	// while (1)
+	// {
+
+	// 	vTaskDelay(50 / portTICK_PERIOD_MS);
+	// }
+	printf("END sd_task\n");
+	vTaskDelete(NULL);
+}
+
+void ILI9341(void *pvParameters)
+{
+	FontxFile fx16G[2];
+	FontxFile fx24G[2];
+	FontxFile fx32G[2];
+	FontxFile fx32L[2];
+	InitFontx(fx16G,"/spiffs/ILGH16XB.FNT",""); // 8x16Dot Gothic
+	InitFontx(fx24G,"/spiffs/ILGH24XB.FNT",""); // 12x24Dot Gothic
+	InitFontx(fx32G,"/spiffs/ILGH32XB.FNT",""); // 16x32Dot Gothic
+	InitFontx(fx32L,"/spiffs/LATIN32B.FNT",""); // 16x32Dot Latinc
+
+	FontxFile fx16M[2];
+	FontxFile fx24M[2];
+	FontxFile fx32M[2];
+	InitFontx(fx16M,"/spiffs/ILMH16XB.FNT",""); // 8x16Dot Mincyo
+	InitFontx(fx24M,"/spiffs/ILMH24XB.FNT",""); // 12x24Dot Mincyo
+	InitFontx(fx32M,"/spiffs/ILMH32XB.FNT",""); // 16x32Dot Mincyo
+    uint16_t model = 0x7735;
+	lcdInit(&dev, model, CONFIG_WIDTH, CONFIG_HEIGHT, CONFIG_OFFSETX, CONFIG_OFFSETY);
+
+	char file[32];
+	lcdFillScreen(&dev,BLACK);
+	uint8_t ascii[24] ;	
+	uint8_t asciiph[24] ;	
+	uint8_t asciitemp[24] ;	
+	uint8_t asciido[24] ;	
+	uint8_t asciiec[24] ;	
+	strcpy(file, "/images/ec.png");
+	PNGTest(&dev, file, 25, 110);
+	strcpy(file, "/images/do.png");
+	PNGTest(&dev, file, 25, 170);
+	strcpy(file, "/images/temp.png");
+	PNGTest(&dev, file, 25, 290);
+	strcpy(file, "/images/ph.png");
+	PNGTest(&dev, file, 25, 230);
+
+	while(1) {
+		
+		
+		
+	
+		
+		// strcpy(file, "/images/san.bmp");
+        // BMPTest(&dev, file, CONFIG_WIDTH, CONFIG_HEIGHT);
+        // WAIT;
+
+		
+				
+				
+        ds3231_get_time(&dev1, &time1);
+		sprintf(time_buff1,"%02d:%02d:%02d", time1.tm_hour, time1.tm_min, time1.tm_sec);
+		lcdDrawFillRect(&dev, 0, 0, 160, 32, BLACK);
+
+		strncpy((char *)ascii, time_buff1, sizeof(ascii) - 1);
+		ascii[sizeof(ascii) - 1] = '\0'; // Đảm bảo kết thúc chuỗi
+		ArrowTest(&dev, fx24G, model, CONFIG_WIDTH, CONFIG_HEIGHT,ascii,17,30 );
+        
+		hieuEC = fabs(checkEC - data.EC);  
+		if(hieuEC == 0){
+		lcdDrawFillRect(&dev, 32, 32, 160, 64, BLACK);
+		sprintf((char *)asciiec, " %.2f ms/cm",data.EC);
+		ArrowTest(&dev, fx16G, model, CONFIG_WIDTH, CONFIG_HEIGHT,asciiec,30,65 );
+		}
+		checkEC = 1000;
+
+		hieuDO = fabs(checkDO - data.DO);
+		if(hieuDO == 0){
+		lcdDrawFillRect(&dev, 32, 64, 160, 96, BLACK);
+		sprintf((char *)asciido, " %.2f mg/l",data.DO);
+		ArrowTest(&dev, fx16G, model, CONFIG_WIDTH, CONFIG_HEIGHT,asciido,30,95 );
+		}
+		checkDO = 1000;
+
+        hieuPH = fabs(checkpH - data.pH);
+		if(hieuPH == 0){
+		lcdDrawFillRect(&dev, 32, 96, 160, 128, BLACK);
+		sprintf((char *)asciiph, " %.2f ",data.pH);
+		ArrowTest(&dev, fx16G, model, CONFIG_WIDTH, CONFIG_HEIGHT,asciiph,30,125 );
+		}
+        checkpH = 1000;
+
+		hieuTemp = fabs(checkTemp - data.temp);
+		if(hieuTemp == 0){
+		lcdDrawFillRect(&dev, 32, 128, 160, 160, BLACK);		
+		sprintf((char *)asciitemp, " %.2f oC",data.temp);
+		ArrowTest(&dev, fx16G, model, CONFIG_WIDTH, CONFIG_HEIGHT,asciitemp,30,155 );	
+		}
+        checkTemp = 1000;	
+				
+		vTaskDelay(905/portTICK_PERIOD_MS);	
+	
+	} 
+	vTaskDelete(NULL);
+}
+
+void task_get_data(void *arg){
+
+   	data.temp = get_Temp(10000.0,3950.0,298.15);
+	printf("Temp :%f \n", data.temp);
+	checkTemp = data.temp;
+    vTaskDelay(3000/portTICK_PERIOD_MS);
+
+	gpio_set_level(pH_ON_PIN1, 1);
+	data.pH = get_pH(nvsHandle, 3300.0, 26400.0);
+	printf("pH :%f\n", data.pH);
+	checkpH = data.pH;
+	gpio_set_level(pH_ON_PIN1, 0);
+    vTaskDelay(3000/portTICK_PERIOD_MS);
+
+	gpio_set_level(EC_ON_PIN1, 1);
+    data.EC = EC_get_value(26400.0,3300.0, 26.0);
+	printf("EC :%f \n",  data.EC);
+	checkEC = data.EC;
+    gpio_set_level(EC_ON_PIN1, 0);
+	vTaskDelay(3000/portTICK_PERIOD_MS);
+
+    gpio_set_level(DO_ON_PIN1, 1);
+	data.DO = get_DO(nvsHandle ,3300.0,  26400.0);
+	printf("DO :%f \n", data.DO);
+	checkDO = data.DO;
+    gpio_set_level(DO_ON_PIN1, 0);
+	vTaskDelay(3000/portTICK_PERIOD_MS);
+
+	xTaskCreate(&GPS, "GPS", 4096, NULL, 5, NULL);  
+    vTaskDelay(4000/portTICK_PERIOD_MS);
+
+	////////
+	hieukd = fabs(checkKD - gps_data.longitude); hieuvd =fabs(checkVD -gps_data.latitude); 
+	if(hieukd <= 0.000165 && hieuvd <= 0.000165){
+	sprintf(buff, "%s|%.2f|%.2f|%.2f|%.2f", time_buff, data.temp, data.pH, data.DO, data.EC);}
+	else{
+    sprintf(buff, "%s|%.2f|%.2f|%.2f|%.2f|%f|%f", time_buff, data.temp, data.pH, data.DO, data.EC,gps_data.latitude,gps_data.longitude);
+	}
+    checkKD = gps_data.longitude; checkVD = gps_data.latitude;
+    ////////
+ //xTaskCreate(ILI9341, "ILI9341", 1024*6, NULL, 2, NULL);  
+//	vTaskDelay(3000/portTICK_PERIOD_MS);
+    xTaskCreatePinnedToCore(send_data_task, "send data uart task", 4096*2, NULL, 32, &send_data, tskNO_AFFINITY);
+	vTaskDelay(2000/portTICK_PERIOD_MS);
+	xTaskCreatePinnedToCore(sd_task, "sd_task", 2048*2, NULL,5, p_sd_card, tskNO_AFFINITY);  
+
+    vTaskDelete(NULL);
+}
+
+void ds3231_task(void *pvParameter)
+{
+	// i2c_dev_t dev;
+	// if (ds3231_init_desc(&dev, 0, 7, 6) != ESP_OK) {
+	// 	//ESP_LOGE(pcTaskGetName(0), "Could not init device descriptor.");
+	// 	while (1) { vTaskDelay(1000); }
+	// }
+	// struct tm time ;
+	// 		// time.tm_year = 2024;
+	// 		// time.tm_mon = 6;
+	// 		// time.tm_mday= 1;
+	// 		// time.tm_hour = 00;
+	// 		// time.tm_min = 05;
+	// 		// time.tm_sec = 10;
+	// 		// ds3231_set_time(&dev, &time);
+	// ds3231_get_time(&dev, &time);
+	ds3231_get_time(&dev1, &time1);
+    sprintf(time_buff,"%04d|%02d|%02d|%02d|%02d|%02d",time1.tm_year, time1.tm_mon, time1.tm_mday, time1.tm_hour, time1.tm_min, time1.tm_sec);
+	printf("%04d-%02d-%02d %02d:%02d:%02d\n", time1.tm_year, time1.tm_mon , time1.tm_mday, time1.tm_hour, time1.tm_min, time1.tm_sec);
+	sprintf(name_file,"log%02d%02d", time1.tm_mon,time1.tm_mday);
+	xTaskCreatePinnedToCore(task_get_data, "get_data", 2048*2, NULL, 1, &get_data, tskNO_AFFINITY);
+	vTaskDelete(NULL);
+
+}
+
+
+void app_main(void)
+{
+///////////////////////////////////////////////////////    INIT	
+	//nvs_flash_init();
+	////DS3231
+	ESP_ERROR_CHECK(i2cdev_init());
+	////UART 
+	UART_init();
+	////GPS
+	GPS_init();
+	////LCD
+
+	ESP_LOGI(TAG, "Initialize NVS");
+	esp_err_t err = nvs_flash_init();
+	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		err = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK( err );
+	ESP_LOGI(TAG, "Initializing SPIFFS");
+	esp_err_t ret;
+	ret = mountSPIFFS("/spiffs", "storage0", 10);
+	if (ret != ESP_OK) return;
+	listSPIFFS("/spiffs/");
+	ret = mountSPIFFS("/icons", "storage1", 10);
+	if (ret != ESP_OK) return;
+	listSPIFFS("/icons/");
+	ret = mountSPIFFS("/images", "storage2", 14);
+	if (ret != ESP_OK) return;
+	listSPIFFS("/images/");
+	
+
+	ESP_LOGI(TAG, "Disable Touch Contoller");
+	int XPT_MISO_GPIO = -1;
+	int XPT_CS_GPIO = -1;
+	int XPT_IRQ_GPIO = -1;
+	int XPT_SCLK_GPIO = -1;
+	int XPT_MOSI_GPIO = -1;
+	spi_master_init(&dev, CONFIG_MOSI_GPIO, CONFIG_SCLK_GPIO, CONFIG_TFT_CS_GPIO, CONFIG_DC_GPIO, 
+	CONFIG_RESET_GPIO, CONFIG_BL_GPIO, XPT_MISO_GPIO, XPT_CS_GPIO, XPT_IRQ_GPIO, XPT_SCLK_GPIO, XPT_MOSI_GPIO);
+	
+	
+
+    ////ADS1115
+    esp_rom_gpio_pad_select_gpio(pH_ON_PIN1);
+	gpio_set_direction(pH_ON_PIN1, GPIO_MODE_OUTPUT);
+    esp_rom_gpio_pad_select_gpio(DO_ON_PIN1);
+	gpio_set_direction(DO_ON_PIN1, GPIO_MODE_OUTPUT);
+    esp_rom_gpio_pad_select_gpio(EC_ON_PIN1);
+	gpio_set_direction(EC_ON_PIN1, GPIO_MODE_OUTPUT);
+  
+    init_param_pH(&nvsHandle);
+    EC_init_param(&nvsHandle);
+	init_param_DO(&nvsHandle);
+    
+	ds3231_init_desc(&dev1, 0, 7, 6);
+	
+			// time.tm_year = 2024;
+			// time.tm_mon = 6;
+			// time.tm_mday= 1;
+			// time.tm_hour = 00;
+			// time.tm_min = 05;
+			// time.tm_sec = 10;
+			// ds3231_set_time(&dev, &time);
+	
+///////////////////////////////////////////////////////////// END INIT
+
+    // Luồng vận hành
+	
+
+	//xTaskCreate(ILI9341, "ILI9341", 1024*6, NULL, 2, NULL);           
+	//vTaskDelay(pdMS_TO_TICKS(6000));                                      //#LCD
+    //xTaskCreatePinnedToCore(sd_task, "sd_task", 2048*2, NULL,5, p_sd_card, tskNO_AFFINITY);                 //#SDCARD
+    //xTaskCreate(&GPS, "GPS", 4096, NULL, 5, NULL);                                                          //#GPS
+    //xTaskCreatePinnedToCore(ds3231_task, "ds3231_task", 2048 * 2, NULL, 4, &test_ds3231, tskNO_AFFINITY);     //#DS3231
+
+	xTaskCreate(ILI9341, "ILI9341", 1024*6, NULL, 2, NULL);  
+    //pH_Calib(3300.0, 26400.0, nvsHandle, "storage", pH_CALIB_9);
+	//EC_calibrate(nvsHandle,3300.0, 26400.0, "storage", 12.88, EC_calib_hight_12_88, 27.0);
+	// DO_Calib(3300.0, 26400.0, nvsHandle,"storage", do_calib_0);
+	// void ILI9341(NULL);    
+	//xTaskCreate(ILI9341, "ILI9341", 1024*6, NULL, 2, NULL);   
+	while (1)
+	{+
+    xTaskCreatePinnedToCore(ds3231_task, "ds3231_task", 2048 * 2, NULL, 4, &test_ds3231, tskNO_AFFINITY); 
+	// xTaskCreate(ILI9341, "ILI9341", 1024*6, NULL, 2, NULL);               
+	// xTaskCreatePinnedToCore(send_data_task, "send_data_task", 2048 * 2, NULL, 4, &send_data, tskNO_AFFINITY);
+    // xTaskCreate(&GPS, "GPS", 4096, NULL, 5, NULL);             
+	vTaskDelay(pdMS_TO_TICKS(60000));
+	ESP_LOGI(TAG, "--------------------------ABC----------------------------\n");
+	}
+	// while(1){
+
+	// data.temp = get_Temp(10000.0,3950.0,298.15);
+	// printf("Temp :%f \n", data.temp);
+	// vTaskDelay(2000/portTICK_PERIOD_MS);
+
+	// gpio_set_level(pH_ON_PIN1, 1);
+	// data.pH = get_pH(nvsHandle, 3300.0, 26400.0);
+	// printf("pH :%f\n", data.pH);
+	// gpio_set_level(pH_ON_PIN1, 0);
+	// vTaskDelay(2000/portTICK_PERIOD_MS);
+
+	// gpio_set_level(EC_ON_PIN1, 1);
+	// data.EC = EC_get_value(26400.0,3300.0, 26.0);
+	// printf("EC :%f \n",  data.EC);
+	// gpio_set_level(EC_ON_PIN1, 0);
+	// vTaskDelay(2000/portTICK_PERIOD_MS);
+
+
+	// gpio_set_level(DO_ON_PIN1, 1);
+	// data.DO = get_DO(nvsHandle ,3300.0,  26400.0);
+	// printf("DO :%f \n", data.DO);
+	// gpio_set_level(DO_ON_PIN1, 0);
+	// vTaskDelay(2000/portTICK_PERIOD_MS);
+
+	//  }
+	
+    
+
+}
